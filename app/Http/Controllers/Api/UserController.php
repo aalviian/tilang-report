3fcd6a7350bbb03b0fe9da9b7c0216d5d8465d72
Muhammad Alvian Supriadi <aalviian@gmail.com>
2017-05-06 22:00:53 +0700

    penambahan API controller

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 
-class UserController extends Controller
+class UserController extends \App\Http\Controllers\Controller
 {
     /**
      * Display a listing of the resource.
